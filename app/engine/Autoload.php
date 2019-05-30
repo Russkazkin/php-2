4c1ab560468d3c2fcac0a0973847bc9b013d77f1
russkazkin <programer.site-uper@yandex.ru>
2019-05-30 12:32:49 +0300

    autoload mk1

--- app/engine/Autoload.php
+++ app/engine/Autoload.php
@@ -8,5 +8,12 @@ class Autoload
 {
     public function loadClass($className){
         var_dump($className);
+
+        $fileName = '/../' . str_replace('\\', '/', $className);
+        var_dump($fileName);
+        require $fileName;
+        if (file_exists($fileName)) {
+            require $fileName;
+        }
     }
 }
--- app/public/index.php
+++ app/public/index.php
@@ -1,6 +1,7 @@
 <?php
 
-require "../engine/Autoload.php";
+require "../config/path.php";
+require ENGINE_DIR . "Autoload.php";
 use engine\Autoload;
 
 spl_autoload_register([new Autoload(), 'loadClass']);
