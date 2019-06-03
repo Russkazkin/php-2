b192b1bca0129ac7ea5278460e2d306ae828247d
russkazkin <programer.site-uper@yandex.ru>
2019-06-03 15:55:58 +0300

    Обновлен дамп базы

--- app/data/shop.sql
+++ app/data/shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: db
--- Время создания: Июн 02 2019 г., 08:56
+-- Время создания: Июн 03 2019 г., 12:54
 -- Версия сервера: 10.3.15-MariaDB-1:10.3.15+maria~bionic
 -- Версия PHP: 7.2.14
 
@@ -21,6 +21,28 @@ SET time_zone = "+00:00";
 --
 -- База данных: `shop`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `manufacturer`
+--
+
+CREATE TABLE `manufacturer` (
+  `id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `description` text NOT NULL,
+  `created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `modified` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `manufacturer`
+--
+
+INSERT INTO `manufacturer` (`id`, `name`, `description`, `created`, `modified`) VALUES
+(1, 'Кубанская буренка', 'Краснодарский молокозавод', '2019-06-03 11:55:37', '2019-06-03 11:55:37'),
+(2, 'Центральный хлебокомбинат', 'Краснодарский хлеб', '2019-06-03 11:55:37', '2019-06-03 11:55:37');
 
 -- --------------------------------------------------------
 
@@ -45,17 +67,25 @@ CREATE TABLE `product` (
 
 INSERT INTO `product` (`id`, `category_id`, `manufacturer_id`, `name`, `description`, `price`, `created`, `modified`) VALUES
 (1, 1, 1, 'Молоко пастеризованное, 1л (полиэтиленовый пакет)', 'Молоко пастеризованное, 1л (полиэтиленовый пакет)', '56.00', '2019-06-02 05:42:13', '2019-06-02 05:42:13'),
-(2, 1, 1, 'Кефир, 1л (полиэтиленовый пакет', '', '49.00', '2019-06-02 05:43:14', '2019-06-02 05:43:14');
+(2, 1, 1, 'Кефир, 1л (полиэтиленовый пакет)', 'Кефир, 1л (полиэтиленовый пакет, жирность 1%)', '49.00', '2019-06-02 05:43:14', '2019-06-03 06:13:48');
 
 --
 -- Индексы сохранённых таблиц
 --
 
 --
+-- Индексы таблицы `manufacturer`
+--
+ALTER TABLE `manufacturer`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Индексы таблицы `product`
 --
 ALTER TABLE `product`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `manufacturer_id` (`manufacturer_id`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
@@ -66,6 +96,16 @@ ALTER TABLE `product`
 --
 ALTER TABLE `product`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`manufacturer_id`) REFERENCES `manufacturer` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
