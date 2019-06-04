7b4ec581c21e45723ab303e4d737c08b7d41b284
russkazkin <programer.site-uper@yandex.ru>
2019-06-04 14:52:01 +0300

    Добавлен роутинг, но не работает

--- app/config/path.php
+++ app/config/path.php
@@ -6,5 +6,6 @@ define("CONFIG_DIR", ROOT_DIR . "config/");
 define("ENGINE_DIR", ROOT_DIR . "engine/");
 define("UPLOADS_DIR", ROOT_DIR . "uploads/");
 define("VENDOR_DIR", ROOT_DIR . "vendor/");
-define("TEMPLATES_DIR", ROOT_DIR . "templates/");
+define("TEMPLATES_DIR", ROOT_DIR . "views/");
+define("LAYOUTS_DIR", TEMPLATES_DIR . "layouts/");
 define('DS', DIRECTORY_SEPARATOR);
--- app/public/index.php
+++ app/public/index.php
@@ -7,7 +7,54 @@ use app\model\{Product, Basket, Manufacturer, Category};
 
 spl_autoload_register([new Autoload(), 'loadClass']);
 
+$page = explode('/', $_SERVER['REQUEST_URI'])[1];
+switch ($page) {
+    case 'index.php';
+    case 'index';
+    case '':
+        $page = 'home';
+        $params = [
+            'title' => 'Home',
+        ];
+        break;
+}
+
+
+
 // phpinfo();
+
+echo render($page, $params, $ajax);
+
+function render($page, $params = [], $ajax = false)
+{
+    if (!$ajax) {
+        $content = renderTemplate(LAYOUTS_DIR . 'main', ['content' => renderTemplate($page, $params)]);
+    } else {
+        $content = json_encode($params, JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK);
+    }
+    return $content;
+}
+
+function renderTemplate($page, $params = [])
+{
+    ob_start();
+    // var_dump($params);
+    if (!is_null($params)) {
+        extract($params);
+    }
+    // var_dump($params);
+    $fileName = TEMPLATES_DIR . $page . ".php";
+    // echo $page;
+    // echo $fileName;
+    // echo TEMPLATES_DIR;
+    if (file_exists($fileName)) {
+        include $fileName;
+    } else {
+        Die("Такой страницы не существует. 404");
+    }
+    return ob_get_clean();
+}
+
 
 echo "<h3>Product</h3>";
 
