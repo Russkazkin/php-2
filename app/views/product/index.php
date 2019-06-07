c8594b8ba9e330eb3fdfd72e0b0f6683808a5503
skazkin <russkazkin@gmail.com>
2019-06-07 13:56:50 +0300

    simple catalog view

--- app/views/product/index.php
+++ app/views/product/index.php
@@ -1,2 +1,22 @@
 <?php
-var_dump($products);
+/**
+ * @var \app\models\Product $products
+ */
+?>
+
+<h1>Каталог</h1>
+
+<div class="products-wrap">
+<?php foreach ($products as $product): ?>
+    <div class="card product-item mb-4" style="width: 18rem;">
+        <img src="/img/<?=$product["img"]?>" class="card-img-top" alt="<?=$product["name"]?> img">
+        <div class="card-body bg-dark">
+            <h5 class="card-title"><?=$product["name"]?></h5>
+            <p class="card-text"><?=$product["description"]?></p>
+            <p class="card-price"><?=$product["price"]?></p>
+            <a href="/product/card/<?=$product["id"]?>" class="btn btn-primary">Купить</a>
+        </div>
+    </div>
+<?php endforeach; ?>
+</div>
+
