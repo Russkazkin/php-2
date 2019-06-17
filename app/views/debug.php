f1ea4f55c17342ac3394c7dee4b7e11c210bf293
skazkin <russkazkin@gmail.com>
2019-06-17 09:57:30 +0300

    CRUD is tested

--- app/views/debug.php
+++ app/views/debug.php
@@ -3,34 +3,16 @@
  * @var Authentication $auth
  */
 use app\engine\Authentication;
+use app\models\repositories\ProductRepository;
 
 $this->title = "Страница отладки";
 
 
-echo "<h3>Authentication</h3>";
+echo "<h3>CRUD test</h3>";
 
-$auth = Authentication::getInstance();
-$password = $auth->passwordHash('user');
-echo $password . '<br>';
-if (isset($_POST['login'])){
-    $auth->userAuth();
-}
-?>
-
-<form method="post">
-    <div class="form-group">
-        <label for="exampleInputEmail1">login</label>
-        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter
-        email" name="login">
-        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-    </div>
-    <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password">
-    </div>
-    <div class="form-group form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1" name="remember" value="remember">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+/**
+ * @var \app\models\entities\Product $pepsi
+ */
+$pepsi = (new ProductRepository())->getOne(105);
+var_dump($pepsi);
+(new ProductRepository())->delete($pepsi);
