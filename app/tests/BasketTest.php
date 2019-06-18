bda5a37ba761e3f96e0bbdc068c93630eab2dbd8
skazkin <russkazkin@gmail.com>
2019-06-18 10:46:18 +0300

    tests suits run

--- app/tests/BasketTest.php
+++ app/tests/BasketTest.php
@@ -10,10 +10,31 @@ use PHPUnit\Framework\TestCase;
 
 class BasketTest extends TestCase
 {
+    protected $fixture;
+
+    protected function setUp() : void
+    {
+        $this->fixture = new Basket('55555', 5, 55);
+    }
+
+    protected function tearDown() : void
+    {
+        $this->fixture = NULL;
+    }
+
+
     public function testObjectCreation(){
-        $basket = new Basket('55555', 5, 55);
+        /**
+         * @var Basket $basket
+         */
+        $basket = $this->fixture;
         $this->assertEquals(55555, $basket->getProp('session_id'));
         $this->assertEquals(5, $basket->getProp('product_id'));
         $this->assertEquals(55, $basket->getProp('user_id'));
+    }
+
+    public function testNamespace(){
+        $this->assertEquals(0, strpos(Basket::class, "App\\"));
+        $this->assertEquals(['models'], array_slice(explode("\\", get_class($this->fixture)), 1, 1));
     }
 }
