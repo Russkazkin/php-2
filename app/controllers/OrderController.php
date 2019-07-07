a00b5c9442cb00afc346049ccd5c9827bfc54921
skazkin <russkazkin@gmail.com>
2019-07-07 07:09:00 +0300

    user_id field added to order

--- app/controllers/OrderController.php
+++ app/controllers/OrderController.php
@@ -11,7 +11,7 @@ class OrderController extends Controller
 {
     public function actionCreate()
     {
-        $order = new Order(0);
+        $order = new Order($this->user_id,0);
         var_dump($order);
         App::call()->orderRepository->save($order);
     }
--- app/models/entities/Order.php
+++ app/models/entities/Order.php
@@ -8,13 +8,15 @@ use app\models\entities\DataEntity;
 
 class Order extends DataEntity
 {
+    protected $user_id;
     protected $status;
     /**
      * Order constructor.
      * @param $status
      */
-    public function __construct($status = null)
+    public function __construct($user_id = null, $status = null)
     {
+        $this->user_id = $user_id;
         $this->status = $status;
 
         parent::__construct();
