5c8c4b90cde65d4909f12080061d4fd2ba093573
skazkin <russkazkin@gmail.com>
2019-07-07 09:56:09 +0300

    order confirmation template

--- app/controllers/OrderController.php
+++ app/controllers/OrderController.php
@@ -11,8 +11,13 @@ class OrderController extends Controller
 {
     public function actionCreate()
     {
+        $this->title = "Order confirmation";
         $order = new Order($this->user_id,0);
-        var_dump($order);
         App::call()->orderRepository->save($order);
+        echo $this->render('order/index', [
+            'heading' => 'Подтверждение заказа',
+            'products' => App::call()->basketRepository->getBasket(session_id(), $this->user_id),
+            'total' => App::call()->basketRepository->getBasketTotal(session_id(), $this->user_id)
+        ]);
     }
 }
